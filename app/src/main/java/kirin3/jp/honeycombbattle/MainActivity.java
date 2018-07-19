800994da475d94e0f3d442b059102da4b42c0543
KIRIN3 <scomputer13@gmail.com>
2018-07-19 23:20:35 +0900

    Init

---
+++ app/src/main/java/kirin3/jp/honeycombbattle/MainActivity.java
@@ -0,0 +1,27 @@
+package kirin3.jp.honeycombbattle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.game_start);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // インテントのインスタンス生成
+//                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                // ゲーム画面の起動
+//                startActivity(intent);
+            }
+        });
+    }
+}
