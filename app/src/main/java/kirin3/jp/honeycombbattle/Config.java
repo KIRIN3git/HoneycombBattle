39f8b70541e6cd8ecbea566e2e17941f0c06d1dd
KIRIN3 <scomputer13@gmail.com>
2019-06-22 00:04:11 +0900

    リリース準備　バグ修正

--- app/src/main/java/kirin3/jp/honeycombbattle/Config.java
+++ app/src/main/java/kirin3/jp/honeycombbattle/Config.java
@@ -2,8 +2,8 @@ package kirin3.jp.honeycombbattle;
 
 public class Config {
 
-    // デバックモード設定
-    public static final boolean IS_DOGFOOD_BUILD = true;
+    // ★デバックモード設定
+    public static final boolean IS_DOGFOOD_BUILD = false;
 
     // ゲームFPS
     public static final long FPS = 60;
